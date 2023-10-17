d7a48b8580bda9a248e51c28cc6bde8807ce0e41
Mjit19 <mjitmukherjee19@gmail.com>
2023-10-17 22:22:30 +0530

    Add button for user prompt
    Now users can input their preferred grid size

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 // Create 16x16 grid square divs
 // Put them inside container div
 
-const container = document.querySelector('#container');
+let container = document.querySelector('#container');
 generateGrid(container, 16);
 
+const btn = document.querySelector('button');
 
+btn.addEventListener('click', () => {
+    let num = prompt("How many squares per side do you want? (1 - 99)");
+    if (num > 0 && num < 100) {
+        container.innerHTML = "";
+        generateGrid(container, num);
+    }
+});
 
 
 
@@ -14,7 +22,7 @@ function generateGrid(parent, n) {
         parent.appendChild(document.createElement('div')); 
     }
     // Create Cells for each row
-    const divContainerList = document.querySelectorAll('#container > div');
+    let divContainerList = document.querySelectorAll('#container > div');
     divContainerList.forEach(div => {
         for (let j = 0; j < n; j++) {
             div.appendChild(document.createElement('div')); 
